a24462ea023f0958a80754faf67f4504587af835
Jack Boyd <j.william.boyd@gmail.com>
2023-06-07 22:21:05 +1200

    clean up t3 create docs as i understand trpc

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -35,21 +35,20 @@ export const postsRouter = createTRPCRouter({
     });
   }),
 
-  create: privateProcedure
-    .input(
-      z.object({
-        content: z.string().emoji().min(1).max(280),
-      })
-    )
-    .mutation(async ({ ctx, input }) => {
-      const authorId = ctx.userId;
-      const post = await ctx.prisma.post.create({
-        data: {
-          authorId,
-          content: input.content,
-        },
-      });
+  create: privateProcedure.input(
+    z.object({
+      content: z.string().emoji().min(1).max(280),
+    })
+  )
+  .mutation(async ({ ctx, input }) => {
+    const authorId = ctx.userId;
+    const post = await ctx.prisma.post.create({
+      data: {
+        authorId,
+        content: input.content,
+      },
+    });
 
-      return post;
-    }),
+    return post;
+  }),
 });
